ab11c98b709731b51fa09068c6afc897252a2918
Lysenko <lysenkoden@gmail.com>
2024-07-05 09:46:39 +0200

    added logic to previous text and refactoring started

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 import db from './db.js';
 
-const olNode = document.getElementById('listOfExamples');
-const btn0Node = document.getElementById('generator0');
-const btn1Node = document.getElementById('generator1');
-const btn2Node = document.getElementById('reset');
-const btn3Node = document.getElementById('proverb');
-const minimumNode = document.getElementById('min');
-const maximumNode = document.getElementById('max');
-const quantityNode = document.getElementById('qty');
+const elements = {
+  olNode: document.getElementById('listOfExamples'),
+  btn0Node: document.getElementById('generator0'),
+  btn1Node: document.getElementById('generator1'),
+  btn2Node: document.getElementById('reset'),
+  btn3Node: document.getElementById('proverb'),
+  minimumNode: document.getElementById('min'),
+  maximumNode: document.getElementById('max'),
+  quantityNode: document.getElementById('qty'),
+};
 
-//1
+const colors = {
+  answer: 'rgb(238, 246, 97)',
+  visited: 'rgb(19, 128, 231)',
+};
+
+//1 addition
 function randomExamples0(min = 1, max = 20, qty = 10) {
   const maxTime = 10000; // 10 seconds in milliseconds
   const startTime = Date.now();
@@ -42,10 +49,10 @@ function randomExamples0(min = 1, max = 20, qty = 10) {
   return arrNew;
 }
 
-btn0Node.addEventListener('click', () => {
-  const minimum = +minimumNode.value || 0;
-  const maximum = +maximumNode.value || 21;
-  const quantity = +quantityNode.value || 10;
+elements.btn0Node.addEventListener('click', () => {
+  const minimum = +elements.minimumNode.value || 0;
+  const maximum = +elements.maximumNode.value || 21;
+  const quantity = +elements.quantityNode.value || 10;
   if (minimum === '' || !maximum || !quantity) {
     alert('some of parameters are not defined');
   }
@@ -53,11 +60,11 @@ btn0Node.addEventListener('click', () => {
     return alert('it is too much for you son...');
   }
 
-  olNode.innerHTML = '';
-  olNode.style.display = 'grid';
+  elements.olNode.innerHTML = '';
+  elements.olNode.style.display = 'grid';
 
   randomExamples0(minimum, maximum, quantity).map((el, index) =>
-    olNode.insertAdjacentHTML(
+    elements.olNode.insertAdjacentHTML(
       'beforeend',
       `<div class='item-example' id='${
         index + 1
@@ -74,45 +81,59 @@ btn0Node.addEventListener('click', () => {
       return i * 100;
     },
   });
+
   //get the answer:
   const exampleNodes = document.querySelectorAll('.item-example');
   exampleNodes.forEach((node) => {
+    let isAnswerShowed = false;
+    // Store the original example text in a data attribute
+    node.dataset.originalText = node.innerText;
+
     node.addEventListener('click', function (event) {
-      if (!/=/.test(event.currentTarget.textContent.toString())) {
-        return;
-      }
       const id = +event.currentTarget.id;
-      const answer =
-        id < 10
-          ? eval(
-              event.currentTarget.textContent
-                .toString()
-                .slice(1)
-                .replace('=', '')
-                .trim()
-            )
-          : id >= 10 && id < 100
-          ? eval(
-              event.currentTarget.textContent
-                .toString()
-                .slice(2)
-                .replace('=', '')
-                .trim()
-            )
-          : eval(
-              event.currentTarget.textContent
-                .toString()
-                .slice(3)
-                .replace('=', '')
-                .trim()
-            );
-      const example = document.getElementById(event.currentTarget.id);
-      example.innerText = answer;
+      isAnswerShowed = !isAnswerShowed;
+
+      const example = document.getElementById(id);
+      const originalExample = example.dataset.originalText;
+
+      if (isAnswerShowed) {
+        const answer =
+          id < 10
+            ? eval(
+                event.currentTarget.textContent
+                  .toString()
+                  .slice(1)
+                  .replace('=', '')
+                  .trim()
+              )
+            : id >= 10 && id < 100
+            ? eval(
+                event.currentTarget.textContent
+                  .toString()
+                  .slice(2)
+                  .replace('=', '')
+                  .trim()
+              )
+            : eval(
+                event.currentTarget.textContent
+                  .toString()
+                  .slice(3)
+                  .replace('=', '')
+                  .trim()
+              );
+        example.innerHTML = `<div class='item-example-num'>${id}</div>${answer}</div>`;
+        example.style.color = colors.answer;
+      } else {
+        example.innerHTML = `<div class='item-example-num'>${id}</div>${originalExample
+          .replace(/^\d+/, '')
+          .trim()}</div>`;
+        example.style.color = colors.visited;
+      }
     });
   });
 });
 
-//2
+//2 multiplication
 function randomExamples1(min = 1, max = 20, qty = 10) {
   const maxTime = 10000; // 10 seconds in milliseconds
   const startTime = Date.now();
@@ -145,10 +166,10 @@ function randomExamples1(min = 1, max = 20, qty = 10) {
   return arrNew;
 }
 
-btn1Node.addEventListener('click', () => {
-  const minimum = +minimumNode.value || 0;
-  const maximum = +maximumNode.value || 21;
-  const quantity = +quantityNode.value || 10;
+elements.btn1Node.addEventListener('click', () => {
+  const minimum = +elements.minimumNode.value || 0;
+  const maximum = +elements.maximumNode.value || 21;
+  const quantity = +elements.quantityNode.value || 10;
   if (minimum === '' || !maximum || !quantity) {
     alert('some of parameters are not defined');
   }
@@ -158,11 +179,11 @@ btn1Node.addEventListener('click', () => {
     );
   }
 
-  olNode.innerHTML = '';
-  olNode.style.display = 'grid';
+  elements.olNode.innerHTML = '';
+  elements.olNode.style.display = 'grid';
 
   randomExamples1(minimum, maximum, quantity).map((el, index) =>
-    olNode.insertAdjacentHTML(
+    elements.olNode.insertAdjacentHTML(
       'beforeend',
       `<div class='item-example' id='${
         index + 1
@@ -183,42 +204,55 @@ btn1Node.addEventListener('click', () => {
   //get the answer:
   const exampleNodes = document.querySelectorAll('.item-example');
   exampleNodes.forEach((node) => {
+    let isAnswerShowed = false;
+    // Store the original example text in a data attribute
+    node.dataset.originalText = node.innerText;
+
     node.addEventListener('click', function (event) {
-      if (!/=/.test(event.currentTarget.textContent.toString())) {
-        return;
-      }
       const id = +event.currentTarget.id;
-      const answer =
-        id < 10
-          ? eval(
-              event.currentTarget.textContent
-                .toString()
-                .slice(1)
-                .replace('=', '')
-                .trim()
-            )
-          : id >= 10 && id < 100
-          ? eval(
-              event.currentTarget.textContent
-                .toString()
-                .slice(2)
-                .replace('=', '')
-                .trim()
-            )
-          : eval(
-              event.currentTarget.textContent
-                .toString()
-                .slice(3)
-                .replace('=', '')
-                .trim()
-            );
-      const example = document.getElementById(event.currentTarget.id);
-      example.innerText = answer;
+      isAnswerShowed = !isAnswerShowed;
+
+      const example = document.getElementById(id);
+      const originalExample = example.dataset.originalText;
+
+      if (isAnswerShowed) {
+        const answer =
+          id < 10
+            ? eval(
+                event.currentTarget.textContent
+                  .toString()
+                  .slice(1)
+                  .replace('=', '')
+                  .trim()
+              )
+            : id >= 10 && id < 100
+            ? eval(
+                event.currentTarget.textContent
+                  .toString()
+                  .slice(2)
+                  .replace('=', '')
+                  .trim()
+              )
+            : eval(
+                event.currentTarget.textContent
+                  .toString()
+                  .slice(3)
+                  .replace('=', '')
+                  .trim()
+              );
+        example.innerHTML = `<div class='item-example-num'>${id}</div>${answer}</div>`;
+        example.style.color = colors.answer;
+      } else {
+        example.innerHTML = `<div class='item-example-num'>${id}</div>${originalExample
+          .replace(/^\d+/, '')
+          .trim()}</div>`;
+        example.style.color = colors.visited;
+      }
     });
   });
 });
 
-//3
+//3 proverb
 function getRandomProverbs(qty = 10) {
   const maxTime = 10000; // 10 seconds in milliseconds
   const startTime = Date.now();
@@ -243,8 +277,8 @@ function getRandomProverbs(qty = 10) {
   return arrNew;
 }
 
-btn3Node.addEventListener('click', () => {
-  const quantity = +quantityNode.value || 10;
+elements.btn3Node.addEventListener('click', () => {
+  const quantity = +elements.quantityNode.value || 10;
   if (quantity <= 0) {
     alert('quantity must be greater than zero');
   }
@@ -252,12 +286,12 @@ btn3Node.addEventListener('click', () => {
     return alert('it is too much for you son... max quantity is 100');
   }
 
-  olNode.style.display = 'flex';
-  olNode.style.flexDirection = 'column';
+  elements.olNode.style.display = 'flex';
+  elements.olNode.style.flexDirection = 'column';
 
-  olNode.innerHTML = '';
+  elements.olNode.innerHTML = '';
   getRandomProverbs(quantity).map((el, index) =>
-    olNode.insertAdjacentHTML(
+    elements.olNode.insertAdjacentHTML(
       'beforeend',
       `<div class='item-example-proverb' id='${
         index + 1
@@ -291,24 +325,23 @@ btn3Node.addEventListener('click', () => {
       const example = document.getElementById(id);
       const originalText = example.dataset.originalText;
 
-      console.log(originalText); //
       if (isAnswerShowed) {
         const proverbText2Find = originalText.replace(/\d/g, '').trim();
         const index = db.findIndex((el) => el.proverb === proverbText2Find);
         example.innerHTML = `<div class='item-example-num'>${id}</div>${db[index].meaning}</div>`;
-        example.style.color = 'rgb(238, 246, 97)';
+        example.style.color = colors.answer;
       } else {
         example.innerHTML = `<div class='item-example-num'>${originalText.match(
           /^\d+/gm
         )}</div>${originalText.replace(/\d/g, '').trim()}</div>`;
-        example.style.color = 'rgb(172, 118, 246)';
+        example.style.color = colors.visited;
       }
     });
   });
 });
 
-//4
-btn2Node.addEventListener('click', () => {
+//4 reset
+elements.btn2Node.addEventListener('click', () => {
   anime({
     targets: '.item-example,.item-example-proverb',
     translateX: [{ value: 2000, duration: 2000 }],
@@ -316,10 +349,10 @@ btn2Node.addEventListener('click', () => {
       return i * 100;
     },
   });
-  minimumNode.value = '';
-  maximumNode.value = '';
-  quantityNode.value = '';
+  elements.minimumNode.value = '';
+  elements.maximumNode.value = '';
+  elements.quantityNode.value = '';
   setTimeout(() => {
-    olNode.innerHTML = '';
+    elements.olNode.innerHTML = '';
   }, 2000);
 });
